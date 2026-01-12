896a53bb6f07db44afa93e11cdb1802050507321
Heitor Westphal <heitordbw2006@gmail.com>
2026-01-12 15:11:17 -0300

    build(android): update package structure and resources

--- android/app/src/main/java/com/aether/ion/app/MainActivity.kt
+++ android/app/src/main/java/com/aether/ion/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ion
+package com.aether.ion.app
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/aether/ion/app/MainApplication.kt
+++ android/app/src/main/java/com/aether/ion/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.ion
+package com.aether.ion.app
 
 import android.app.Application
 import android.content.res.Configuration
